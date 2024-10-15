b1f2d3f315e4bdf6632ba25b3cadf023308942ee
Rikthi Pranadhik Mondal <147805511+rikthi@users.noreply.github.com>
2024-10-15 13:26:45 -0600

    No js in app.jsx

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ import Counter from "./Counter";
 import React from "react";
 
 function App() {
-  const eventsList = [
-    { id: 20241, name: "White Festival", date: "25.11.2024", joined: false },
-    { id: 20242, name: "Black Festival", date: "29.10.2024", joined: false },
-    { id: 20243, name: "Yellow Festival", date: "31.12.2024", joined: true },
-  ];
   return (
     <div>
       {/* <Event name="Winter Festival" date="25.11.2024" willJoin={false}></Event>
